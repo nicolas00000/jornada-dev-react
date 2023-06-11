491f8b108561b432d9971281800425399ff3778b
nicolas00000 <nicmariano10@gmail.com>
2023-06-11 19:37:45 -0300

    chamando a funcao

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Button from  './components/button'
+import Condicional from './components/condicionais';
 
 
 
 function App() {
-  function ativarAlerta(){
-    alert("fui cluicado")
-  }
-
   return (
     <div className="App">
-      <Button event={ativarAlerta} text="clique em mim para ativar uma função"/>
+      <Condicional/>
     </div>
   );
 }
